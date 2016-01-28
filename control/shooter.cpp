6872c2102150b17f33bdd6f5ee599578aa9bfb9b
Software1 <nothing@nothin.com>
2016-01-27 18:50:54 -0800

    Add shooter, update TODO

---
+++ control/shooter.cpp
@@ -0,0 +1,11 @@
+
+#ifdef SHOOTER_TEST
+#include "formal.h"
+
+int main(){
+	Shooter a;
+	tester(a);
+}
+
+#endif
+
---
+++ control/shooter.h
@@ -0,0 +1,16 @@
+#ifndef SHOOTER_H
+#define SHOOTER_H
+
+#include <iostream>
+#include <set>
+#include "../util/interface.h"
+#include "../util/util.h"
+#include "../util/driver_station_interface.h"
+
+struct Shooter{
+	enum class Goal{}
+
+}
+
+
+#endif
