d6aa8c59fc02327d9b25d0dd2bb8321e096831a7
rince_charming <nothing@nothin.com>
2016-01-28 20:33:28 -0800

    Add three-position switches and swtich between auto and manual mode to panel

--- input/panel.cpp
+++ input/panel.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 Panel::Panel():
 	in_use(0),
-	control_tilt(0),
+	control_angle(0),
 	eject(0),
 	collect(0),
 	reflect(0),
@@ -60,7 +60,7 @@ ostream& operator<<(ostream& o, Panel::Auto_mode a){
 ostream& operator<<(ostream& o,Panel p){
 	o<<"Panel(";
 	o<<"in_use:"<<p.in_use;
-	o<<", control_tilt:"<<p.control_tilt;
+	o<<", control_angle:"<<p.control_angle;
 	o<<", eject:"<<p.eject;
 	o<<", collect:"<<p.collect;
 	o<<", reflect:"<<p.reflect;
@@ -89,7 +89,7 @@ Panel::Auto_mode auto_mode_convert(int potin){
 }
 
 Panel interpret(Joystick_data d){
-	#define AXIS_RANGE(axis, last, curr, next, button) if (axis > curr-(curr-last)/2 && axis < curr+(next-curr)) button = 1;
+	#define AXIS_RANGE(axis, last, curr, next, var, val) if (axis > curr-(curr-last)/2 && axis < curr+(next-curr)) var = val;
 	Panel p;
 	{
 		p.in_use=[&](){
@@ -106,18 +106,49 @@ Panel interpret(Joystick_data d){
 		Volt auto_mode=d.axis[0];
 		p.auto_mode=auto_mode_convert(interpret_10_turn_pot(auto_mode));
 	}
+	p.collector_auto = d.button[0];
 	{
-		float op=d.axis[1];
+		float op = d.axis[1];
 		static const float DEFAULT=-1, COLLECT=-.7, REFLECT=-.4, EJECT=0, STOW=.4, PORTCULLIS=.7, CHEVAL=1;
 		#define X(button) p.button = 0;
 		X(collect) X(reflect) X(eject) X(stow) X(open_portcullis) X(lower_cheval)
 		#undef X
-		AXIS_RANGE(op, DEFAULT, COLLECT, REFLECT, p.collect)
-		else AXIS_RANGE(op, COLLECT, REFLECT, EJECT, p.reflect)
-		else AXIS_RANGE(op, REFLECT, EJECT, STOW, p.eject)
-		else AXIS_RANGE(op, EJECT, STOW, PORTCULLIS, p.stow)
-		else AXIS_RANGE(op, STOW, PORTCULLIS, CHEVAL, p.open_portcullis)
-		else AXIS_RANGE(op, PORTCULLIS, CHEVAL, 1.5, p.lower_cheval)
+		AXIS_RANGE(op, DEFAULT, COLLECT, REFLECT, p.collect, 1)
+		else AXIS_RANGE(op, COLLECT, REFLECT, EJECT, p.reflect, 1)
+		else AXIS_RANGE(op, REFLECT, EJECT, STOW, p.eject, 1)
+		else AXIS_RANGE(op, EJECT, STOW, PORTCULLIS, p.stow, 1)
+		else AXIS_RANGE(op, STOW, PORTCULLIS, CHEVAL, p.open_portcullis, 1)
+		else AXIS_RANGE(op, PORTCULLIS, CHEVAL, 1.5, p.lower_cheval, 1)
+	}
+	if (p.collector_auto) {
+		{
+			float climb = d.axis[3];
+			static const float RETRACT=-1, STOP=0, EXTEND=1;
+			p.climber = Panel::Climber::RETRACT;
+			AXIS_RANGE(climb, RETRACT, STOP, EXTEND, p.climber, Panel::Climber::STOP)
+			else AXIS_RANGE(climb, STOP, EXTEND, 1.5, p.climber, Panel::Climber::EXTEND)
+		}
+		{
+			float front = d.axis[8];
+			static const float IN=-1, OFF=0, OUT=1;
+			p.front = Panel::Collector::IN;
+			AXIS_RANGE(front, IN, OFF, OUT, p.front, Panel::Collector::OFF)
+			else AXIS_RANGE(front, OFF, OUT, 1.5, p.front, Panel::Collector::OUT)
+		}
+		{
+			float sides = d.axis[6];
+                        static const float IN=-1, OFF=0, OUT=1;
+                        p.sides = Panel::Collector::IN;
+                        AXIS_RANGE(sides, IN, OFF, OUT, p.sides, Panel::Collector::OFF)
+                        else AXIS_RANGE(sides, OFF, OUT, 1.5, p.sides, Panel::Collector::OUT)
+		}
+		{
+			float tilt = d.axis[5];
+			static const float DOWN=-1, STOP=0, UP=1;
+			p.tilt = Panel::Tilt::DOWN;
+			AXIS_RANGE(tilt, DOWN, STOP, UP, p.tilt, Panel::Tilt::STOP)
+			else AXIS_RANGE(tilt, STOP, UP, 1.5, p.tilt, Panel::Tilt::UP);
+		}
 	}
 	#undef AXIS_RANGE
 	return p;
