804f7829df8c6e0baf22a1b971e5aec232983360
Software1 <1425sw@gmail.com>
2016-04-21 20:43:17 -0700

    some small fixes

--- control/drivebase.h
+++ control/drivebase.h
@@ -84,6 +84,6 @@ bool operator!=(Drivebase::Estimator const&,Drivebase::Estimator const&);
 bool operator!=(Drivebase const&,Drivebase const&);
 std::ostream& operator<<(std::ostream&,Drivebase const&);
 
-float range(const Robot_inputs);
+float range(const Robot_inputs); //To be used for wall following NOT DONE 
 
 #endif
