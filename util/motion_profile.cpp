b4a79306c30e4085381b93fb02e54a3175ee02ec
software1 <1425sw@gmail.com>
2016-08-04 21:08:00 -0700

    Add files for motion profiling

---
+++ util/motion_profile.cpp
@@ -0,0 +1,9 @@
+#include "motion_profile.h"
+
+#ifdef MOTION_PROFILE_TEST
+
+int main(){
+	return 0;
+}
+
+#endif
---
+++ util/motion_profile.h
@@ -0,0 +1,8 @@
+#ifndef MOTION_PROFILE_H
+#define MOTION_PROFILE_H
+
+struct Motion_profile{
+
+};
+
+#endif
