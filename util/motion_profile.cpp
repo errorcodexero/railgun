09f3288ef128a94364a484e51edabf4e3f109120
Wonderwoman <nothing@nothin.com>
2016-08-11 18:52:37 -0700

    added some starter code for the motion profile system

--- util/motion_profile.cpp
+++ util/motion_profile.cpp
@@ -1,8 +1,32 @@
 #include "motion_profile.h"
+#include <iostream>
 
 #ifdef MOTION_PROFILE_TEST
+float speed_distance_calculation(int goal, int current,float k){
+	int error;
+	float motorv;
+	float clipped;
+	float max=1;
 
+	error=goal-current;
+	motorv=error*k;
+	
+	if(motorv>max) clipped=max;
+
+	else if(motorv<-max) clipped=-max;
+
+	else clipped = motorv;
+	
+	return clipped;
+
+
+}
 int main(){
+	int g = 100;
+	int c = 70;
+	float k =.01;
+	std::cout << "speed: " << speed_distance_calculation(g,c,k) << std::endl;
+	
 	return 0;
 }
 
