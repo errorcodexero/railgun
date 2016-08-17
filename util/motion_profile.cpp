99988f437da84654bbc310d6663c5daa88f52b1a
software1 <1425sw@gmail.com>
2016-08-16 20:33:37 -0700

    Work on motion profiling

--- util/motion_profile.cpp
+++ util/motion_profile.cpp
@@ -2,50 +2,55 @@
 #include <iostream>
 #include <assert.h>
 
-#ifdef MOTION_PROFILE_TEST
-
 template<class T, class Compare>
 const T& clamp( const T& v, const T& lo, const T& hi, Compare comp )
 {
-    return assert( !comp(hi, lo) ),
-        comp(v, lo) ? lo : comp(hi, v) ? hi : v;
+	return assert( !comp(hi, lo) ),
+	comp(v, lo) ? lo : comp(hi, v) ? hi : v;
 }
 
 template<class T>
 const T& clamp( const T& v, const T& lo, const T& hi )
 {
-    return clamp( v, lo, hi, std::less<T>() );
+	return clamp( v, lo, hi, std::less<T>() );
 }
 
-float speed_distance_calculation(float goal, float current,float k){
+double Motion_profile::target_speed(const double current){
 	float error = goal-current;
-	float motorv = error*k;
+	float motorv = error*vel_modifier;
 	const float max=1;
 	
 	return clamp(motorv,-max,max);
-	
 }
 
+Motion_profile::Motion_profile():goal(0),vel_modifier(.02){}
+Motion_profile::Motion_profile(double g, double m):goal(g),vel_modifier(m){}
+
+#ifdef MOTION_PROFILE_TEST
+
 int main(){
-	int g = 100;//goal distance
 	int c = 10;//current distance
-	float k =.02;//a=value that is used to modify the speed
 	float dis=0; //feet
-	const float target = 20; //units in feet
-	const int speed = 5; //feet/.5 seconds
-	
+	const float target = 20; //feet
+	const int speed = 10; //feet per second
 
-	std::cout << "speed: " << speed_distance_calculation(g,c,k) << std::endl;
+	Motion_profile mp1(100, .02);	
 
-	for(int i=0;i<=100;i+=10) std::cout << "speedlist " << i << ": " << speed_distance_calculation(g,i,k) << std::endl;
+	std::cout << "speed: " << mp1.target_speed(c) << std::endl;
+
+	for(int i=0;i<=100;i+=10) std::cout << "speedlist " << i << ": " << mp1.target_speed(i) << std::endl;
 	
-	for(float i=0;i<60;i+=.5){//i is a unit of seconds
-		dis += speed*speed_distance_calculation(target,dis,k);
-		std::cout << "Time in seconds: " << i << " Distance in feet: " << dis << " of " << target << std::endl;
+	Motion_profile mp2(target, .02);
+
+	const float INCREMENT=.5;//seconds
+		
+	for(float i=0;i<60;i+=INCREMENT){//i is in seconds
+		std::cout << "Time(seconds):" << i << "    Current Distance(feet):" << dis << "    Target Distance(feet):" << target << "    "<<mp2.target_speed(dis)<<std::endl;
 		if(dis>=(target-1)){
 			std::cout << "Robot has reached the target range" << std::endl;
 			break;
 		}
+		dis += INCREMENT*speed*mp2.target_speed(dis);
 	}
 	
 	return 0;
--- util/motion_profile.h
+++ util/motion_profile.h
@@ -2,7 +2,13 @@
 #define MOTION_PROFILE_H
 
 struct Motion_profile{
-
+	double goal;//feet
+	double vel_modifier;
+	
+	double target_speed(const double);
+	
+	Motion_profile();
+	Motion_profile(double,double);	
 };
 
 #endif
