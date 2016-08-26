2a2ed4f93586fd37266af3676b0dbdd378be2e32
software1 <1425sw@gmail.com>
2016-08-25 21:01:03 -0700

    Add files for auto test mode

---
+++ modes/auto_test.h
@@ -0,0 +1,11 @@
+#ifndef AUTO_TEST_H
+#define AUTO_TEST_H
+
+#include "mode.h"
+
+struct Auto_test: public Mode{
+	unique_ptr<Mode> next_mode(Next_mode_info);
+	Toplevel::Goal run(Run_info);
+};
+
+#endif
