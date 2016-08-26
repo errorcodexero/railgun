15fc6e2ea34a316bd1dce1b1e5485318e9366c33
Software2 <1425sw@gmail.com>
2016-08-25 21:04:28 -0700

    Add to Next_mode_info and Run_info

--- modes/mode.h
+++ modes/mode.h
@@ -3,13 +3,25 @@
 
 #include <memory>
 #include "../control/toplevel.h"
+#include "../input/panel.h"
+#include "../util/interface.h"
 
 struct Next_mode_info {
-	
+	bool autonomous, autonomous_start;
+	Toplevel::Status_detail const status;
+	Time since_switch;
+	Panel panel;
+	bool const toplready;
+	Robot_inputs const in;
+	pair<int,int> initial_encoders;
 };
 
 struct Run_info {
-
+	Robot_inputs const& in;
+	Joystick_data const& main_joystick, gunner_joystick;
+	Panel const& panel;
+	Toplevel::Status_detail const& toplevel_status;
+	Tilt::Goal level, low, top, cheval, drawbridge;
 };
 
 struct Mode {
