df309d427c34c8061828e268c839a8f59502c113
Software2 <1425sw@gmail.com>
2016-09-06 20:08:14 -0700

    Add motion profiling into barrelracer files

--- modes/barrelracer.cpp
+++ modes/barrelracer.cpp
@@ -8,7 +8,7 @@ unique_ptr<Mode> Auto_br_straightaway::next_mode(Next_mode_info info){
 	if(!info.autonomous) return make_unique<Teleop>();
 	const double TARGET_DISTANCE = 5.0*12.0;//inches
 	const double TOLERANCE = 6.0;//inches
-	//motion_profile.set_goal(TARGET_DISTANCE);
+	motion_profile.set_goal(TARGET_DISTANCE);
 	cout<<"\n"<<encoder_differences.first<<"   "<<ticks_to_inches(encoder_differences.first)<<"   "<<TARGET_DISTANCE<<"\n";
 	if(ticks_to_inches(encoder_differences.first) >= TARGET_DISTANCE-TOLERANCE && ticks_to_inches(encoder_differences.first) <= TARGET_DISTANCE+TOLERANCE){
 		in_br_range.update(info.in.now,info.in.robot_mode.enabled);
@@ -19,14 +19,14 @@ unique_ptr<Mode> Auto_br_straightaway::next_mode(Next_mode_info info){
 	if(in_br_range.done()){
 		//set_initial_encoders=false;
 		return make_unique<Teleop>();
-		//return make_unique<Auto_br_initialturn>();
+		//return make_unique<Auto_br_initialturn>();//TODO 
 	}
-	return make_unique<Auto_br_straightaway>(br_step);
+	return make_unique<Auto_br_straightaway>(br_step,motion_profile);
 }
 
-Toplevel::Goal Auto_br_straightaway::run(Run_info){
+Toplevel::Goal Auto_br_straightaway::run(Run_info info){
 	Toplevel::Goal goals;
-	double power=0;//-info.motion_profile.target_speed(ticks_to_inches(info.toplevel_status.drive.ticks.first));
+	double power=-motion_profile.target_speed(ticks_to_inches(info.toplevel_status.drive.ticks.first));
 	goals.drive.left=power;
 	goals.drive.right=power;
 	return {};
@@ -37,7 +37,7 @@ bool Auto_br_straightaway::operator==(Auto_br_straightaway const& a)const{
 }
 
 unique_ptr<Mode> Auto_br_initialturn::next_mode(Next_mode_info){
-	return make_unique<Auto_br_initialturn>(br_step);
+	return make_unique<Auto_br_initialturn>(br_step,motion_profile);
 }
 
 Toplevel::Goal Auto_br_initialturn::run(Run_info){
@@ -49,7 +49,7 @@ bool Auto_br_initialturn::operator==(Auto_br_initialturn const& a)const{
 }
 
 unique_ptr<Mode> Auto_br_side::next_mode(Next_mode_info){
-	return make_unique<Auto_br_side>(br_step);
+	return make_unique<Auto_br_side>(br_step,motion_profile);
 }
 
 Toplevel::Goal Auto_br_side::run(Run_info){
@@ -61,7 +61,7 @@ bool Auto_br_side::operator==(Auto_br_side const& a)const{
 }
 
 unique_ptr<Mode> Auto_br_sideturn::next_mode(Next_mode_info){
-	return make_unique<Auto_br_sideturn>(br_step);
+	return make_unique<Auto_br_sideturn>(br_step,motion_profile);
 }
 
 Toplevel::Goal Auto_br_sideturn::run(Run_info){
@@ -73,7 +73,7 @@ bool Auto_br_sideturn::operator==(Auto_br_sideturn const& a)const{
 }
 
 unique_ptr<Mode> Auto_br_endturn::next_mode(Next_mode_info){
-	return make_unique<Auto_br_endturn>(br_step);
+	return make_unique<Auto_br_endturn>(br_step,motion_profile);
 }
 
 Toplevel::Goal Auto_br_endturn::run(Run_info){
@@ -93,7 +93,7 @@ int main(){
 		X(Auto_br_sideturn) \
 		X(Auto_br_endturn)
 
-	#define X(NAME) { NAME a(0); test_mode(a); }
+	#define X(NAME) { NAME a(0,{}); test_mode(a); }
 	STEPS
 	#undef X
 }
